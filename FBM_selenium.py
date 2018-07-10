471a153753218e5fe8b26bf0e579911690f83e9b
Kiwibp <keenan.burkepitts@gmail.com>
2018-07-10 14:24:55 -0400

    type:docs - removed irrelevant files and updated README file

--- FBM_selenium.py
+++ FBM_selenium.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from pymongo import MongoClient
 
 class App:
-    def __init__(self, email= "keenan.burkepitts@gmail.com", password= "!1Testing45", 
+    def __init__(self, email= "", password= "", 
                  path='/Users/keenanburkepitts/Documents/Data_Science/NYC_DSA/final_project'):
         self.email = email
         self.password = password
@@ -21,7 +21,7 @@ class App:
         self.used_item_links = []
         self.scrape_item_links()
         self.scrape_item_details()
-        #self.driver.quit()
+        self.driver.quit()
         
         
     def log_in(self):
@@ -58,68 +58,70 @@ class App:
             for i in range(100):
                 try:
                     self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-                    sleep(1)
+                        sleep(1.5)
                 except:
                     pass
                 
             full_items_list = self.driver.find_elements_by_xpath("//a[@class='_1oem']")
-            if len(self.used_item_links) == 0: #False or None?
+            if self.used_item_links == 0: #False or None?
                 self.used_item_links = [item.get_attribute('href') for item in full_items_list]
             else:
-                #append or extend or add?
-                self.used_item_links.extend([item.get_attribute('href') for item in full_items_list])
+                    #append or extend or add?
+                self.used_item_links = self.used_item_links.extend([item.get_attribute('href') for item in full_items_list])
             
-
-            print([item.get_attribute('href') for item in full_items_list])        
             
-
+            #wait = WebDriverWait(self.driver, 10)
+            #category_element = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, category.get_attribute('class'))))
+            #category_element.click()
+            print([item.get_attribute('href') for item in full_items_list])
+        
+        print(self.used_item_links)
         return self.used_item_links
         
 
     
-    def scrape_item_details(self):         
-        db = self.client.LOCAL_USED_ITEMS       
-        for url in self.used_item_links:
+    def scrape_item_details(self, used_item_links):
+        db = LOCAL_USED_ITEMS
+        for url in used_item_links:
             images = []
-            self.driver.get(url)
+            driver.get(url)
             sleep(0.5)
-		
+
+            url = url
             try:
-                image_element = self.driver.find_element_by_xpath('//img[contains(@class, "_5m")]')
+                image_element = driver.find_element_by_xpath('//img[contains(@class, "_5m")]')
                 images = [image_element.get_attribute('src')]
             except:
                 images = ""
             try:
-                title = self.driver.find_element_by_xpath('//div[contains(@class, " _50f")]/span').text
+                title = driver.find_element_by_xpath('//div[contains(@class, " _50f")]/span').text
             except:
                 title = ""
             try:
-                date_time = self.driver.find_element_by_xpath('//a[@class="_r3j"]').text
+                date_time = driver.find_element_by_xpath('//a[@class="_r3j"]').text
             except:
                 date_time = ""
             try:
-                location =  self.driver.find_element_by_xpath('//span[@class="_7yi"]').text
+                location =  driver.find_element_by_xpath('//span[@class="_7yi"]').text
             except:
                 location = ""
             try:
-                price = self.driver.find_element_by_xpath('//div[contains(@class, "_5_md")]').text
+                price = driver.find_element_by_xpath('//div[contains(@class, "_5_md")]').text
             except:
                 price = ""
             try:
-                if self.driver.find_element_by_xpath("//a[@title='More']").is_displayed():
-                    self.driver.find_element_by_xpath("//a[@title='More']").click()
-                    description = self.driver.find_element_by_xpath('//p[@class="_4etw"]/span').text
-                else:
-                    description = self.driver.find_element_by_xpath('//p[@class="_4etw"]/span').text
+                if driver.find_element_by_xpath("//a[@title='More']").is_displayed():
+                    driver.find_element_by_xpath("//a[@title='More']").click()
+                description = driver.find_element_by_xpath('//p[@class="_4etw"]/span').text
             except:
                 description = ""
-		
+
             try:
-                previous_and_next_buttons = self.driver.find_elements_by_xpath("//i[contains(@class, '_3ffr')]")
+                previous_and_next_buttons = driver.find_elements_by_xpath("//i[contains(@class, '_3ffr')]")
                 next_image_button = previous_and_next_buttons[1]
                 while next_image_button.is_displayed():
                     next_image_button.click()
-                    image_element = self.driver.find_element_by_xpath('//img[contains(@class, "_5m")]')
+                    image_element = driver.find_element_by_xpath('//img[contains(@class, "_5m")]')
                     sleep(1)   
                     if image_element.get_attribute('src') in images:
                         break
@@ -127,81 +129,6 @@ class App:
                         images.append(image_element.get_attribute('src'))
             except:
                 pass
-     
-             
-                 
-            db.Facebook_items.insert_one({ 'Url': url,
-                         'Images': images,
-                         'Title': title,
-                         'Description': description,
-                         'Date_Time': date_time,
-                         'Location': location,
-                         'Price': price,          
-                        })
-                 
-            print({ 'Url': url,
-                         'Images': images,
-                         'Title': title,
-                         'Description': description,
-                         'Date_Time': date_time,
-                         'Location': location,
-                         'Price': price,
-                        })
-
-'''
-def scrape_item_details(used_item_links):
-	#finish this function
-	client = MongoClient('mongodb://localhost:27017/')
-	db = LOCAL_USED_ITEMS
-    for url in used_item_links:
-		images = []
-		driver.get(url)
-        sleep(0.5)
-		
-		url = url
-		try:
-			image_element = driver.find_element_by_xpath('//img[contains(@class, "_5m")]')
-			images = [image_element.get_attribute('src')]
-		except:
-			images = ""
-		try:
-			title = driver.find_element_by_xpath('//div[contains(@class, " _50f")]/span').text
-		except:
-			title = ""
-		try:
-			date_time = driver.find_element_by_xpath('//a[@class="_r3j"]').text
-		except:
-			date_time = ""
-		try:
-			location =  driver.find_element_by_xpath('//span[@class="_7yi"]').text
-		except:
-			location = ""
-		try:
-			price = driver.find_element_by_xpath('//div[contains(@class, "_5_md")]').text
-		except:
-			price = ""
-		try:
-			if driver.find_element_by_xpath("//a[@title='More']").is_displayed():
-				driver.find_element_by_xpath("//a[@title='More']").click()
-                description = driver.find_element_by_xpath('//p[@class="_4etw"]/span').text
-			description = driver.find_element_by_xpath('//p[@class="_4etw"]/span').text
-		except:
-            description = ""
-		
-		try:
-			previous_and_next_buttons = driver.find_elements_by_xpath("//i[contains(@class, '_3ffr')]")
-            next_image_button = previous_and_next_buttons[1]
-            while next_image_button.is_displayed():
-				next_image_button.click()
-                image_element = driver.find_element_by_xpath('//img[contains(@class, "_5m")]')
-                sleep(1)   
-                if image_element.get_attribute('src') in images:
-					break
-                else:
-                    images.append(image_element.get_attribute('src'))
-     
-        except:
-            pass
      
              
                  
@@ -223,7 +150,7 @@ def scrape_item_details(used_item_links):
                          'Price': price,
                         })
 
-'''      
+
 
 if __name__ == '__main__':
     app = App()
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# NYC-DSA-Bootcamp----Final-Project# NYC-DSA-Bootcamp--Final-Project
+# Data Wrangling & NLP on Local Used Items
+---
+## Overview
+I wrangled 10,000 local used items around me from Craigslist.org, letgo.com, and Facebook Marketplace.  I then performed EDA with Python and Tableau. I also built a few simple extractor and classification NLP models using MonkeyLearn API. You can view the interactive visualizations I created in Tableau here: https://public.tableau.com/profile/keenan.burke.pitts#!/vizhome/NYCDSAFinalProject_0/LocalUsedItemsAnalysis
+## Installation
+You will need to download the Scrapy Framework https://scrapy.org/ to scrape items from letgo.com and craigslist.org.  You may need a paid crawlera account https://scrapinghub.com/crawlera to avoid getting blocked from Craigslist. You will need to use Selenium https://selenium-python.readthedocs.io/ for scraping Facebook Marketplace. You will need to download Tableau software https://public.tableau.com/en-us/s/download for the visualizations or you can simply interact with them on my public profile.  You will also need an account with MonkeyLearn https://monkeylearn.com/ to connect with their API to execute the NLP models.
